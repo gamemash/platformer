dd35c44221afe9552729fd814df9924c19bf712c
Ronald Hecker <ronald.m.hecker@gmail.com>
2016-04-09 18:22:56 +0200

    Background transparent

--- src/webgl_renderer.js
+++ src/webgl_renderer.js
@@ -22,10 +22,11 @@ let WebGLRenderer = stampit()
   })
   .init(function(){
     this.scene = new THREE.Scene();
-    this.renderer = new THREE.WebGLRenderer({canvas: this.canvas});
+    this.renderer = new THREE.WebGLRenderer({canvas: this.canvas, alpha: true});
     this.width = this.renderer.domElement.offsetWidth;
     this.height = this.renderer.domElement.offsetHeight;
     this.renderer.setSize(this.width, this.height);
+    this.renderer.setClearColor(0x5c94fc, 0);
     this.camera = new THREE.OrthographicCamera(-this.width / 2, this.width / 2, this.height / 2, this.height / 2, 0.1, 100 );
   });
 
