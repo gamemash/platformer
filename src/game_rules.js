87269b0bf162522489b3b17a634db0d7e6efa1f8
Oliver Ponder <oliver.ponder@gmail.com>
2016-04-12 00:07:24 +0200

    Let the player 'burst' into the world upon death

--- src/game_rules.js
+++ src/game_rules.js
@@ -1,12 +1,23 @@
 let stampit = require('stampit');
 let sounds = require('./sounds.js');
-
+let dead = false;
 let GameRules = stampit.compose().refs().init().methods({
   update: function(player, game) {
     if (player.position.y < -2) {
-      console.log('dead');
-      sounds.die.play();
-      game.paused = true;
+      if (!dead){
+        sounds.die.play();
+        window.setTimeout(function() {
+          player.position.x = 6;
+          player.position.y = 5;
+          player.acceleration.y = 0;
+          player.velocity.y = 18;
+          player.velocity.x = 2;
+          sounds.kick.play();
+          dead = false;
+        }, 3000)
+      }
+
+      dead = true;
     }
   }
 });
