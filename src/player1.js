d655bc4fc9ab8e30a9afb87fe61150d62e0058b0
Ronald Hecker <ronald.m.hecker@gmail.com>
2016-04-11 21:46:10 +0200

    Move binding box to entity

--- src/player1.js
+++ src/player1.js
@@ -24,12 +24,6 @@ let Player1 = stampit.compose(Mario, Entity)
   .init(function(){
   })
   .methods({
-    boundingBox: function(obj_a, obj_b){
-      return (obj_a.position.x < obj_b.position.x + obj_b.size &&
-         obj_a.position.x + obj_a.size > obj_b.position.x &&
-         obj_a.position.y < obj_b.position.y + obj_b.size &&
-         obj_a.size + obj_a.position.y > obj_a.position.y);
-    },
     update: function(dt){
       this.acceleration.x = 0;
       this.acceleration.y = -this.gravity / this.mass;
