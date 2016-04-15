8ca33e478f2458c2748eef46f7e1f6ae9d041628
Ronald Hecker <ronald.m.hecker@gmail.com>
2016-04-15 23:28:23 +0200

    Extract level

--- main.js
+++ main.js
@@ -7,14 +7,12 @@ let Player1             = require('./src/player1.js');
 let Selector            = require('./src/selector.js');
 let MouseState          = require('./src/mouse_state.js');
 let MouseStream         = require('./src/mouse_stream.js');
-let Debug               = require('./src/debug.js');
-let {Goomba}            = require('./src/enemies.js')
 let gameRules           = require('./src/game_rules.js').create();
 let gameState           = require('./src/game_state.js');
 let gameStateController = require('./src/game_state_controller.js');
-let levelData           = require('./src/level_data.js');
 let Interface           = require('./src/interface.js');
 let Game                = require('./src/game.js');
+let Level               = require('./src/levels/level_1.js');
 
 let {inputStream, menuStream, jumpStream, inputState} = require("./src/input_stream.js");
 
@@ -23,7 +21,7 @@ let game = Game.create();
 let renderer = WebGLRenderer.create({canvas: document.getElementById('game-canvas')});
 
 game.renderer = renderer;
-game.loadLevel(levelData['1-1']);
+game.loadLevel(Level.create());
 let player1  = Player1.create({game: game, position: new THREE.Vector2(5, 4) });
 let gui      = Interface.create({game: game, player: player1, gameRules: gameRules});
 game.player = player1;
--- src/game.js
+++ src/game.js
@@ -1,11 +1,8 @@
 let stampit             = require('stampit');
 let THREE               = require('three');
-let {ItemBlock, Ground, Block, Brick, PipeTopLeft, PipeTopRight, PipeBottomLeft, PipeBottomRight} = require('./blocks.js');
 let gameState           = require('./game_state.js');
 let gameRules           = require('./game_rules.js').create();
-let {Goomba}            = require('./enemies.js')
 
-let Blocks = [undefined, Ground, Block, Brick, PipeTopLeft, PipeTopRight, PipeBottomLeft, PipeBottomRight, ItemBlock];
 var Game = stampit()
   .refs({
     entities: []
@@ -14,23 +11,9 @@ var Game = stampit()
     start: function(){
       this.render();
     },
-    loadLevel: function(levelData){
-      for (let y = levelData.length - 1; y >= 0; y -= 1){
-        for (let x = 0; x < levelData[y].length; x += 1){
-          if (levelData[y][x] != 0) {
-            let block = Blocks[levelData[y][x]].create({game: this, position: new THREE.Vector2(x, levelData.length - 1 - y)});
-            if (block.animated) {
-              this.renderer.toUpdate.add(block);
-            }
-          }
-        }
-      }
-
-      for(var i=-2; i<3; i++) {
-        let goom = Goomba.create({game: this, position: new THREE.Vector2(20+i*2, 6)})
-        this.entities.push(goom);
-      }
-
+    loadLevel: function(level){
+      this.level = level;
+      this.level.load(this);
     },
     render: function(){
       let dt = 1/60;
