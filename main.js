aaf1c9251c8d0fd8b380b159081e42c9b7177d66
Oliver Ponder <oliver.ponder@gmail.com>
2016-04-15 14:34:02 +0200

    Line things up all fancy and stuff

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 "use strict";
 
-let stampit = require('stampit');
-let THREE = require('three');
-let WebGLRenderer = require('./src/webgl_renderer.js');
-let Player1 = require('./src/player1.js');
-let Selector = require('./src/selector.js');
-let MouseState = require('./src/mouse_state.js');
-let MouseStream = require('./src/mouse_stream.js');
-let Debug = require('./src/debug.js');
-let {Goomba} = require('./src/enemies.js')
-let gameRules = require('./src/game_rules.js').create();
-let gameState = require('./src/game_state.js');
+let stampit             = require('stampit');
+let THREE               = require('three');
+let WebGLRenderer       = require('./src/webgl_renderer.js');
+let Player1             = require('./src/player1.js');
+let Selector            = require('./src/selector.js');
+let MouseState          = require('./src/mouse_state.js');
+let MouseStream         = require('./src/mouse_stream.js');
+let Debug               = require('./src/debug.js');
+let {Goomba}            = require('./src/enemies.js')
+let gameRules           = require('./src/game_rules.js').create();
+let gameState           = require('./src/game_state.js');
 let gameStateController = require('./src/game_state_controller.js');
-let levelData = require('./src/level_data.js');
+let levelData           = require('./src/level_data.js');
 
 let renderer = WebGLRenderer.create({canvas: document.getElementById('game-canvas')});
 let player1  = Player1.create({renderer: renderer, position: new THREE.Vector2(5, 4) });
